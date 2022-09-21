ea7abd37c53f0804c9401123bcf72f4c04b1fab6
slothmanxyz <99208989+dyk3878@users.noreply.github.com>
2022-09-21 15:14:01 -0700

    ..

--- dist/timer/instructions/collectDepositInstruction.d.ts
+++ dist/timer/instructions/collectDepositInstruction.d.ts
@@ -1,3 +1,3 @@
 import { CofiSolanaConfig } from '../../types';
 import { web3 } from '@project-serum/anchor';
-export declare function collectDepositInstruction(cofiSolanaConfig: CofiSolanaConfig, timerOwnedAccount: web3.PublicKey, destinationLiquidityAccount?: web3.PublicKey): Promise<web3.TransactionInstruction>;
+export declare function collectDepositInstruction(cofiSolanaConfig: CofiSolanaConfig, timerOwnedAccount: web3.PublicKey, destinationLiquidityAccount: web3.PublicKey): Promise<web3.TransactionInstruction>;
