826826c0af81d27bb3332763f082293a26ae19a2
slothmanxyz <99208989+dyk3878@users.noreply.github.com>
2022-10-06 12:13:59 -0700

    rent exempt values added

--- dist/utils/constants.d.ts
+++ dist/utils/constants.d.ts
@@ -1,2 +1,5 @@
 import { BN } from "@project-serum/anchor";
 export declare const WAD: BN;
+export declare const COFI_ACCOUNT_RENT_EXEMPT: BN;
+export declare const COFI_STAKE_RENT_EXEMPT: BN;
+export declare const COFI_TIMER_RENT_EXEMPT: BN;
--- dist/utils/constants.js
+++ dist/utils/constants.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.WAD = void 0;
+exports.COFI_TIMER_RENT_EXEMPT = exports.COFI_STAKE_RENT_EXEMPT = exports.COFI_ACCOUNT_RENT_EXEMPT = exports.WAD = void 0;
 const anchor_1 = require("@project-serum/anchor");
 exports.WAD = new anchor_1.BN('1000000000000000000');
+exports.COFI_ACCOUNT_RENT_EXEMPT = new anchor_1.BN("5178200");
+exports.COFI_STAKE_RENT_EXEMPT = new anchor_1.BN("5080800");
+exports.COFI_TIMER_RENT_EXEMPT = new anchor_1.BN("5310480");
