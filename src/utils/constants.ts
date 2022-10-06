4edcba2e481d496cc45760fc6f9cb8bc39bd37b5
slothmanxyz <99208989+dyk3878@users.noreply.github.com>
2022-10-06 12:07:04 -0700

    rent exempt values added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,7 @@
 import { BN } from "@project-serum/anchor";
 
 export const WAD = new BN('1000000000000000000');
+
+export const COFI_ACCOUNT_RENT_EXEMPT = new BN("5178200")
+export const COFI_STAKE_RENT_EXEMPT = new BN("5080800")
+export const COFI_TIMER_RENT_EXEMPT = new BN("5310480")
