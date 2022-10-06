f3f671cdbe23238248a87d0ca2a6dcb3d79c8fe6
slothmanxyz <99208989+dyk3878@users.noreply.github.com>
2022-10-06 12:18:42 -0700

    rent exempt values added

--- dist/utils/constants.d.ts
+++ dist/utils/constants.d.ts
@@ -3,3 +3,4 @@ export declare const WAD: BN;
 export declare const COFI_ACCOUNT_RENT_EXEMPT: BN;
 export declare const COFI_STAKE_RENT_EXEMPT: BN;
 export declare const COFI_TIMER_RENT_EXEMPT: BN;
+export declare const SPL_ACCOUNT_RENT_EXEMPT: BN;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,4 @@ export const WAD = new BN('1000000000000000000');
 export const COFI_ACCOUNT_RENT_EXEMPT = new BN("5178200")
 export const COFI_STAKE_RENT_EXEMPT = new BN("5080800")
 export const COFI_TIMER_RENT_EXEMPT = new BN("5310480")
+export const SPL_ACCOUNT_RENT_EXEMPT = new BN("2040000")
